fda644e335464ab322a90d901dff821bd7fbc80c
delanoquirino <delanoquirino@gmail.com>
2023-01-28 15:38:38 -0300

    creating component repo

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import { Box, Button, Text } from "@chakra-ui/react";
+import { BsCodeSlash } from "react-icons/bs";
+import { AiOutlineStar, AiOutlineFork } from "react-icons/ai";
+import { RiGitRepositoryLine } from "react-icons/ri";
+import { RepoProps } from "../types/repo";
+import { Link } from "react-router-dom";
 
-export const Ropo = () => {
-  return <div>Ropo</div>;
+export const Repo = ({
+  name,
+  language,
+  html_url,
+  forks_count,
+  stargazers_count,
+}: RepoProps) => {
+  return (
+    <Box>
+      <Box textStyle="h3">{name}</Box>
+      <Text>
+        <BsCodeSlash /> {language}
+      </Text>
+      <Box>
+        <Box>
+          <AiOutlineStar />
+          <Text>{stargazers_count}</Text>
+        </Box>
+        <Box>
+          <AiOutlineFork />
+          <Text>{forks_count}</Text>
+        </Box>
+      </Box>
+      <Link to={html_url} target="_blank">
+        <Button>Ver código</Button>
+        <RiGitRepositoryLine />
+      </Link>
+    </Box>
+  );
 };
